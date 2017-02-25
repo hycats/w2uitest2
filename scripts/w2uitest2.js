e72cdd559f5c98d22ac5b81f50a447f078cf0d6c
Atsushi Hayashi <hycats@gmail.com>
2017-02-25 23:52:48 +0900

    add amcharts

--- scripts/w2uitest2.js
+++ scripts/w2uitest2.js
@@ -91,6 +91,31 @@ jQuery(function ($) {
         rowHeaderWidth: 150,
         readOnly: true
     });
+
+    // チャート
+    AmCharts.makeChart("chartdiv", {
+        type: 'serial',
+        dataProvider: [
+            { year: 2013, income: 500000, outgo: 450000 },
+            { year: 2014, income: 320000, outgo: 680000 },
+            { year: 2015, income: 660000, outgo: 720000 },
+            { year: 2016, income: 120000, outgo: 345000 },
+            { year: 2017, income: 800000, outgo: 450000 },
+        ],
+        categoryField: 'year',
+        categoryAxis: {
+            gridPosition: 'start'
+        },
+        valueAxes: [{
+            minimum: 0
+        }],
+        graphs: [
+            { valueField: 'income', type: 'column', fillAlphas: 0.8, balloonText: '[[category]]: <b>[[value]]</b>' },
+            { valueField: 'outgo', type: 'column', fillAlphas: 0.8, balloonText: '[[category]]: <b>[[value]]</b>' }
+        ],
+        angle: 30,
+        depth3D: 15
+    });
 });
 /*
 var mymodule = angular.module('myApp', ['ngSanitize']);
